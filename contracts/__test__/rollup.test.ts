cac43c6481ccca80145358eb512871f0f370a8ce
Kendrick Tan <kendricktan0814@gmail.com>
2020-01-25 01:12:53 +1000

    more tests

--- contracts/__test__/rollup.test.ts
+++ contracts/__test__/rollup.test.ts
@@ -19,7 +19,7 @@ describe("Rollup.sol", () => {
   const depth = 4;
   const zeroValue = bigInt(0);
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     try {
       const circomLibContract = await deployCircomLib();
       const hasherContract = await deployHasher(circomLibContract.address);
@@ -101,5 +101,40 @@ describe("Rollup.sol", () => {
     );
 
     expect(bigInt(newUserData[3].toString())).toEqual(bigInt(0));
+  });
+
+  it("Multiple Users, multiple deposits", async () => {
+    const privA = genPrivateKey();
+    const pubA = genPublicKey(privA);
+
+    const privB = genPrivateKey();
+    const pubB = genPublicKey(privB);
+
+    // User A deposits 1 eth
+    await rollUpContract.deposit(pubA[0].toString(), pubA[1].toString(), {
+      //@ts-ignore
+      value: "0x" + toWei(1.0).toString(16)
+    });
+
+    // User B deposits 2 eth
+    await rollUpContract.deposit(pubB[0].toString(), pubB[1].toString(), {
+      //@ts-ignore
+      value: "0x" + toWei(2.0).toString(16)
+    });
+
+    // User A deposits 0.5 eth
+    await rollUpContract.deposit(pubA[0].toString(), pubA[1].toString(), {
+      //@ts-ignore
+      value: "0x" + toWei(0.5).toString(16)
+    });
+
+    // Get User A Data
+    const userAData = await rollUpContract.getUserData(
+      pubA[0].toString(),
+      pubA[1].toString()
+    );
+
+    // Expect balance
+    expect(bigInt(userAData[3].toString())).toEqual(toWei(1.5));
   });
 });
